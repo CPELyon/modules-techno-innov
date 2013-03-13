531b70f516d6470017b56a9749958a0aecb79d64
Nathael Pajani <nathael.pajani@ed3l.fr>
2013-03-13 16:27:08 +0100

    Adding Byte swap instructions support.

--- include/core/lpc_core_cm0.h
+++ include/core/lpc_core_cm0.h
@@ -142,6 +142,35 @@ static inline void set_fault_mask(uint32_t mask)
 
 
 /*******************************************************************************/
+/*            Byte swap instructions                                           */
+/*******************************************************************************/
+/* Swap bytes of each 16-bit halfword in a 32-bit word, keeping halfword order */
+static inline uint32_t double_byte_swap_16(volatile uint32_t value)
+{
+	uint32_t result = 0;
+	__asm volatile ("rev16 %0, %1" : "=r" (result) : "r" (value));
+	return result;
+}
+/* Change endianness of a 16-bit halfword */
+static inline uint32_t byte_swap_16(volatile uint16_t value)
+{
+	uint32_t result = 0;
+	__asm volatile ("rev16 %0, %1" : "=r" (result) : "r" (value));
+	return (result & 0xFFFF);
+}
+/* Change endianness of a 32-bit word */
+static inline uint32_t byte_swap_32(volatile uint32_t value)
+{
+	uint32_t result = 0;
+	__asm volatile ("rev %0, %1" : "=r" (result) : "r" (value));
+	return result;
+}
+
+
+
+
+
+/*******************************************************************************/
 /*            Interrupts                                                       */
 /*******************************************************************************/
 

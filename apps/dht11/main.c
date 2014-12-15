18f0cd6f46c776222bb12c1b4b9826e2bc2cc284
Nathael Pajani <nathael.pajani@ed3l.fr>
2014-12-15 15:05:36 +0100

    First application : support for the DHT11 sensor

---
+++ apps/dht11/main.c
@@ -0,0 +1,103 @@
+/****************************************************************************
+ *   apps/dht11/main.c
+ *
+ *  DHT11 example
+ *
+ * Copyright 2013-2014 Nathael Pajani <nathael.pajani@ed3l.fr>
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************************************************************************** */
+
+
+#include <stdint.h>
+#include "core/lpc_regs_12xx.h"
+#include "core/lpc_core_cm0.h"
+#include "core/pio.h"
+#include "core/system.h"
+#include "lib/stdio.h"
+#include "drivers/serial.h"
+#include "drivers/gpio.h"
+#include "extdrv/status_led.h"
+#include "extdrv/dht11.h"
+
+
+#define SELECTED_FREQ  FREQ_SEL_48MHz
+
+/***************************************************************************** */
+/* Pins configuration */
+/* pins blocks are passed to set_pins() for pins configuration.
+ * Unused pin blocks can be removed safely with the corresponding set_pins() call
+ * All pins blocks may be safelly merged in a single block for single set_pins() call..
+ */
+const struct pio_config common_pins[] = {
+	/* UART 0 */
+	{ LPC_UART0_RX_PIO_0_1,  LPC_IO_DIGITAL },
+	{ LPC_UART0_TX_PIO_0_2,  LPC_IO_DIGITAL },
+	ARRAY_LAST_PIO,
+};
+
+const struct pio dht11_gpio = LPC_GPIO_0_6; /* Used for DTH11 */
+
+
+void system_init()
+{
+	/* Stop the watchdog */
+	stop_watchdog(); /* Do it right now, before it gets a chance to break in */
+
+	/* Note: Brown-Out detection must be powered to operate the ADC. adc_on() will power
+	 *  it back on if called after system_init() */
+	system_brown_out_detection_config(0);
+	system_set_default_power_state();
+	clock_config(SELECTED_FREQ);
+	set_pins(common_pins);
+	gpio_on();
+	status_led_config();
+	/* System tick timer MUST be configured and running in order to use the sleeping
+	 * functions */
+	systick_timer_on(1); /* 1ms */
+	systick_start();
+}
+
+/* Define our fault handler. This one is not mandatory, the dummy fault handler 
+ * will be used when it's not overridden here.
+ * Note : The default one does a simple infinite loop. If the watchdog is deactivated
+ * the system will hang.
+ */
+void fault_info(const char* name, uint32_t len)
+{
+	serial_write(0, name, len);
+	/*FIXME : wait for end of Tx and perform soft reset of the micro-controller ! */
+}
+
+
+/***************************************************************************** */
+int main(void) {
+	system_init();
+	uart_on(0, 115200, NULL);
+
+	/* Configure the DHT11 and the onboard temp sensor */
+	dht11_config(&dht11_gpio);
+
+	while (1) {
+		dht11_display(0, 1);
+		chenillard(50);
+	}
+	return 0;
+}
+
+
+
+

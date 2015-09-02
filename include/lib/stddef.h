9d3682773377a14651264f1e2dd2a986ed086442
Nathael Pajani <nathael.pajani@ed3l.fr>
2015-09-02 11:08:07 +0200

    Provide offsetof when there is no builtin Add container_of

--- include/lib/stddef.h
+++ include/lib/stddef.h
@@ -22,6 +22,24 @@
 #ifndef LIB_STDDEF_H
 #define LIB_STDDEF_H
 
-#define offsetof(type, member)  __builtin_offsetof (type, member)
+/**
+ * offsetof - return the offset of a member in the containing structure.
+ */
+#ifdef __builtin_offsetof
+ #define offsetof(type, member)  __builtin_offsetof (type, member)
+#else
+ #define offsetof(type, member) ((size_t) &((type *)0)->member)
+#endif
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ */
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+
 
 #endif /* LIB_STDDEF_H */
